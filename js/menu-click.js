dff5348bc84a5aa00349e6eef5da7a152aeecf6d
James Brooks <jamescbrooks2020@gmail.com>
2021-10-20 07:32:48 +1100

    Created drop down dynamic menu for mobile.

--- js/menu-click.js
+++ js/menu-click.js
@@ -1,8 +1,27 @@
-const navBarElement = document.querySelector("#nav-bar");
+/* Create seperate working document to compile sections together */
+const docFrag = document.createDocumentFragment();
+
+/* Scan all section headers to prepare for menu click */
+const sectionList = document.querySelectorAll(".section h1");
+
+for(let i = 0; i < sectionList.length; i++) {
+    const listItemElement = document.createElement("li");
+    listItemElement.innerText = sectionList[i].innerText;
+    docFrag.appendChild(listItemElement);
+    console.log(docFrag);
+
+}
+
+/* Finalise the ready menu div item */
+let menuElement = document.createElement("div");
+menuElement.id = "menu";
+menuElement.className = "box";
+const menuElementList = document.createElement("ul");
+menuElementList.appendChild(docFrag);
+menuElement.appendChild(menuElementList);
 
 const menuToggle = () => {
     let cssLinkElement = document.querySelector("link");
-    let menuElement = null;
 
     if (cssLinkElement.href === "http://localhost:3000/css/landing-page-combined-menu-down.css") {
         cssLinkElement.href = "/css/landing-page-combined.css";
@@ -13,26 +32,14 @@ const menuToggle = () => {
         cssLinkElement.href = "/css/landing-page-combined-menu-down.css";
 
         const headerElement = document.querySelector("#header");
-        menuElement = document.createElement("div");
-        menuElement.id = "menu";
-        menuElement.className = "box";
-        const menuListElement
-        
-        for(let i = 0; i < allHeaders.length; i++){
-            menuElement.innerHTML = "<ul>"
-        }
-
-        `<div id="menu" class="box">
-        MENU
-        </div>`;
-        headerElement.insertAdjacentHTML("afterend", menuElement);
+        headerElement.insertAdjacentElement("afterend", menuElement);
 
     }
 
 }
 
-/* Scan all headers to prepare for menu click */
-const sectionList = document.querySelectorAll(".section");
+/* Assign nav-bar element on page */
+const navBarElement = document.querySelector("#nav-bar");
 
 /* Wait for click on menu */
 navBarElement.addEventListener('click', menuToggle);
