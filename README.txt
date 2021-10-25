916a3d3ad9a27cb402047c6ec329c30d5c54697e
James Brooks <jamescbrooks2020@gmail.com>
2021-10-26 09:42:27 +1100

    Allowed for changing active state with scroll.

---
+++ README.txt
@@ -0,0 +1,15 @@
+Landing Page Example - James Brooks
+
+--Installation--
+This webpage works best when run on a server, but works perfectly fine run locally by opening the index.html file into a browser.
+
+--Usage--
+Open the index.html file using a browser. Using the navigation bar in the top right, click to show a menu of sections. Click the section you want to scroll to and it will highlight the button and section selected.
+
+More sections can be added to the html file as needed and the nav-bar will dynaically update itself for easy addition to the website.
+
+--Development--
+Made using VS Code with the help of gulp and browser-sync to allow faster debugging and dev.
+
+--Author--
+Made by James Brooks 22/10/2021
--- js/app.js
+++ js/app.js
@@ -5,19 +5,46 @@ const docFrag = document.createDocumentFragment();
 const sectionList = document.querySelectorAll(".section h1");
 
 /* Set active state function */
-const setActiveState = (num) => {
+const setActiveStateOnClick = (num) => {
     if (document.querySelector(".active")) {
         document.querySelector(".active").classList.remove("active");
-        setActiveState(num);
+        setActiveStateOnClick(num);
 
     } else {
         const activeDiv = document.getElementById(`section-${num}`);
         const activeButton = document.getElementById(`button-${num}`)
         activeDiv.classList.add("active");
-        activeButton.classList.add("active");
+        activeButton.classList.add("active-menu");
 
     }
     
+
+}
+
+/* Set active on Scroll */
+const setActiveStateOnScroll = () => {
+    let activeDiv = document.getElementsByClassName("section active")[0];
+    let activeDivPreSibling = activeDiv.previousElementSibling;
+    let activeDivNxtSibling = activeDiv.nextElementSibling;
+    if (activeDivNxtSibling !== null && activeDivNxtSibling.getBoundingClientRect().top < window.screen.height) {
+        activeDiv = activeDivNxtSibling;
+        if (document.querySelector(".active-menu") !== null){
+            document.querySelector(".active-menu").classList.remove("active-menu");
+
+        }
+        document.getElementsByClassName("section active")[0].classList.remove("active");
+        activeDiv.classList.add("active");
+            
+    } else if (activeDivPreSibling !== null && activeDivPreSibling.getBoundingClientRect().bottom > 0) {
+        activeDiv = activeDivPreSibling;
+        if (document.querySelector(".active-menu") !== null){
+            document.querySelector(".active-menu").classList.remove("active");
+
+        }
+        document.getElementsByClassName("section active")[0].classList.remove("active-menu");
+        activeDiv.classList.add("active");
+
+    }
 
 }
 
@@ -38,11 +65,10 @@ for(let i = 0; i < sectionList.length; i++) {
     const listItemElement = document.createElement("li");
     const listItemElementText = document.createElement("button");
     listItemElementText.id = `button-${(i+1)}`
-    listItemElementText.setAttribute("onclick",`scrollToSection(${(i+1)}); setActiveState(${(i+1)})`);
+    listItemElementText.setAttribute("onclick",`scrollToSection(${(i+1)}); setActiveStateOnClick(${(i+1)})`);
     listItemElementText.innerText = sectionList[i].innerText;
     listItemElement.appendChild(listItemElementText);
     docFrag.appendChild(listItemElement);
-    console.log(docFrag);
 
 }
 
@@ -74,3 +100,6 @@ const navBarElement = document.querySelector("#nav-bar");
 
 /* Wait for click on menu */
 navBarElement.addEventListener('click', menuToggle);
+
+/* Wait for scolling to change active section */
+document.addEventListener("scroll", setActiveStateOnScroll);
